e94d53392bd456057f3faf748418db2562314eee
maksim <knyazev.maksim2852@gmail.com>
2025-11-28 01:27:09 +0400

    обновлен базовый юз кейс, добавлен - обновление роли у пользователя

--- src/use_cases/admin/team_manager.py
+++ src/use_cases/admin/team_manager.py
@@ -5,8 +5,10 @@ from src.repositories.user import BaseUserRepository
 from src.schemas.user import UserOut
 from src.models.team import TeamModel
 from src.models.user import UserModel
-from src.exceptions.user import UserNotFoundException, AlreadyInTeamException, UserNotInTeamException
+from src.exceptions.user import UserNotFoundException, AlreadyInTeamException, UserNotInTeamException, InvalidRoleException
 from src.exceptions.team import TeamNotFoundException
+from src.schemas.user import UserRole
+
 
 
 @dataclass
@@ -17,7 +19,7 @@ class BaseTeamMemberUseCase(ABC):
     async def _validate_and_get_model(self, user_id: int, team_id: int) -> tuple[UserModel, TeamModel]:
 
         user: UserModel | None = await self._user_repository.get_by_id(id=user_id)
-
+        
         if not user:
             raise UserNotFoundException()
         
@@ -27,6 +29,11 @@ class BaseTeamMemberUseCase(ABC):
             raise TeamNotFoundException()
         
         return user, team
+
+    async def _save_and_return_user(self, user: UserModel) -> UserOut:
+
+        updated_user: UserModel = await self._user_repository.save(user=user)
+        return UserOut.model_validate(updated_user)
     
 
 
@@ -42,9 +49,7 @@ class AddTeamMemberUseCase(BaseTeamMemberUseCase):
             raise AlreadyInTeamException()
         
         user.team_id = team.id
-        user: UserModel = await self._user_repository.save(user=user)
-
-        return UserOut.model_validate(user)
+        return await self._save_and_return_user(user)
 
 
 
@@ -59,5 +64,20 @@ class DeleteTeamMemberUseCase(BaseTeamMemberUseCase):
             raise UserNotInTeamException()
         
         user.team_id = None
-        user: UserModel = await self._user_repository.save(user=user)
-        return UserOut.model_validate(user)
+        return await self._save_and_return_user(user)
+
+@dataclass
+class ChangeUserRoleUseCase(BaseTeamMemberUseCase):
+
+    async def execute(self, user_id: int, team_id: int, new_role: UserRole) -> UserOut:
+
+        user, team = await self._validate_and_get_model(user_id, team_id)
+
+        if user.team_id != team.id:
+            raise UserNotInTeamException()
+        
+        if new_role not in [UserRole.USER, UserRole.MANAGER]:
+            raise InvalidRoleException()
+        
+        user.role = new_role
+        return await self._save_and_return_user(user)
