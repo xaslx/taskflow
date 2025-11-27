410b8fdec74dd844aa7773ab5ecab8da67538931
maksim <knyazev.maksim2852@gmail.com>
2025-11-28 00:16:01 +0400

    удален admin роутер, перенесён эндпоинт в users

--- src/routers/users.py
+++ src/routers/users.py
@@ -1,9 +1,11 @@
-from fastapi import APIRouter, status
+from typing import Annotated
+from fastapi import APIRouter, Path, status
 from dishka.integrations.fastapi import inject, FromDishka as Depends
 from src.models.user import UserModel
-from src.schemas.user import UserUpdateSchema, UserOut
+from src.schemas.user import AdminUserOut, UserUpdateSchema, UserOut
 from src.use_cases.user.update_user import UpdateUserUseCase
 from src.use_cases.user.delete import DeleteUserUseCase
+from src.use_cases.admin.delete_user import DeleteUserByAdminUseCase
 
 
 router: APIRouter = APIRouter()
@@ -62,3 +64,27 @@ async def delete_user(
     use_case: Depends[DeleteUserUseCase]
 ) -> None:
     await use_case.execute(user=user)
+
+
+
+
+@router.delete(
+    '/users/{user_id}',
+    description='[ADMIN] Удаление пользователя по его ID.',
+    summary='Удалить пользователя',
+    status_code=status.HTTP_204_NO_CONTENT,
+    responses={
+        status.HTTP_204_NO_CONTENT: {'description': 'Пользователь удален'},
+        status.HTTP_401_UNAUTHORIZED: {'description': 'Не авторизован'},
+        status.HTTP_403_FORBIDDEN: {'description': 'Недостаточно прав. Только для администраторов'},
+        status.HTTP_404_NOT_FOUND: {'description': 'Пользователь не найден'},
+    },
+)
+@inject
+async def delete_user_by_admin(
+    user_id: Annotated[int, Path(description='ID пользователя, которого нужно удалить')],
+    admin: Depends[AdminUserOut],
+    use_case: Depends[DeleteUserByAdminUseCase],
+) -> None:
+    
+    await use_case.execute(user_id=user_id)
