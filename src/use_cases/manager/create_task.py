f3cddeb014cae78bc40e6e28dec651dec1f35393
maksim <knyazev.maksim2852@gmail.com>
2025-11-28 03:52:08 +0400

    добавлены юз кейсы для создания задач и получения списка

---
+++ src/use_cases/manager/create_task.py
@@ -0,0 +1,37 @@
+from dataclasses import dataclass
+from src.models.task import TaskModel
+from src.models.team import TeamModel
+from src.models.user import UserModel
+from src.repositories.task import BaseTaskRepository
+from src.repositories.team import BaseTeamRepository
+from src.repositories.user import BaseUserRepository
+from src.schemas.task import TaskCreate, TaskOut
+from src.exceptions.team import TeamNotFoundException
+from src.exceptions.user import UserNotFoundException, UserNotInTeamException
+
+
+@dataclass
+class CreateTaskUseCase:
+    _task_repository: BaseTaskRepository
+    _team_repository: BaseTeamRepository
+    _user_repository: BaseUserRepository
+
+    async def execute(self, task: TaskCreate, author_id: int) -> TaskOut:
+
+        team: TeamModel | None = await self._team_repository.get_by_id(task.team_id)
+
+        if not team:
+            raise TeamNotFoundException()
+        
+
+        author: UserModel | None = await self._user_repository.get_by_id(author_id)
+
+        if not author:
+            raise UserNotFoundException()
+
+        if author.team_id != team.id:
+            raise UserNotInTeamException()
+        
+
+        task_model: TaskModel = await self._task_repository.add(task, author_id)
+        return TaskOut.model_validate(task_model)
---
+++ src/use_cases/manager/get_all_tasks.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+from src.repositories.task import BaseTaskRepository
+from src.models.task import TaskModel
+from src.schemas.task import TaskOut
+
+
+@dataclass
+class GetAllTasksUseCase:
+    _task_repository: BaseTaskRepository
+
+    async def execute(self, team_id: int) -> list[TaskOut]:
+
+        tasks: list[TaskModel] = await self._task_repository.get_all_by_team_id(team_id=team_id)
+
+        return [TaskOut.model_validate(task) for task in tasks]
