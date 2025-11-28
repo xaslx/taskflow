65d56f5d61524cd1e453233f571a3d6d6d15d2d5
maksim <knyazev.maksim2852@gmail.com>
2025-11-29 00:53:51 +0400

    обновлено

--- src/database/postgres.py
+++ src/database/postgres.py
@@ -1,8 +1,7 @@
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine, AsyncEngine
 from src.config import Config
 
-
-def new_session_maker(config: Config) -> async_sessionmaker[AsyncSession]:
+def new_engine_and_session_maker(config: Config) -> tuple[async_sessionmaker[AsyncSession], AsyncEngine]:
     database_uri = f'postgresql+asyncpg://{config.postgres_user}:' \
                    f'{config.postgres_password}@{config.postgres_host}:' \
                    f'{config.postgres_port}/{config.postgres_db}'
@@ -12,6 +11,9 @@ def new_session_maker(config: Config) -> async_sessionmaker[AsyncSession]:
         pool_size=15,
         max_overflow=15,
     )
-    return async_sessionmaker(
+
+    session_maker = async_sessionmaker(
         engine, class_=AsyncSession, autoflush=False, expire_on_commit=False
     )
+
+    return session_maker, engine
