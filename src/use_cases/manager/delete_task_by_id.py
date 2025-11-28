440d69d59a7024d4450b1c180293000a5008d35f
maksim <knyazev.maksim2852@gmail.com>
2025-11-28 04:45:15 +0400

    добавлены эндпоинты для получения и удаления задачи по ID

---
+++ src/use_cases/manager/delete_task_by_id.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+from src.models.task import TaskModel
+from src.repositories.task import BaseTaskRepository
+from src.exceptions.task import TaskNotFoundException
+from src.exceptions.user import ForbiddenException
+
+@dataclass
+class DeleteTaskByIdUseCase:
+    _task_repository: BaseTaskRepository
+    
+    async def execute(self, task_id: int, user_id: int, user_team_id: int) -> None:
+
+        task: TaskModel | None = await self._task_repository.get_by_id(id=task_id)
+
+        if not task:
+            raise TaskNotFoundException()
+
+
+        if task.team_id != user_team_id:
+            raise ForbiddenException()
+
+
+        if task.author_id != user_id:
+            raise ForbiddenException()
+
+        await self._task_repository.delete(task=task)
---
+++ src/use_cases/manager/get_by_task_by_id.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+from src.schemas.task import TaskOut
+from src.models.task import TaskModel
+from src.repositories.task import BaseTaskRepository
+
+
+
+
+@dataclass
+class GetTaskByIdUseCase:
+    _task_repository: BaseTaskRepository
+    
+    async def execute(self, task_id: int, team_id: int) -> TaskOut | None:
+
+        task: TaskModel | None = await self._task_repository.get_by_id(id=task_id)
+
+        if not task or task.team_id != team_id:
+            return None
+        return TaskOut.model_validate(task)
