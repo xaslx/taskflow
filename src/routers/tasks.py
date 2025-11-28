acbcfa052cadc492dc60459080b661a0c6072071
maksim <knyazev.maksim2852@gmail.com>
2025-11-28 06:00:40 +0400

    добавлены эндпоинты для обновления задачи и добавления комментариев к залдаче

--- src/routers/tasks.py
+++ src/routers/tasks.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, status, Path
-from src.schemas.task import TaskCommentCreate, TaskCreate, TaskOut, TaskUpdate
+from src.models.user import UserModel
+from src.schemas.task import TaskCommentCreate, TaskCommentOut, TaskCreate, TaskOut, TaskUpdate
 from dishka.integrations.fastapi import inject, FromDishka as Depends
 from src.schemas.user import ManagerUserOut
 from src.use_cases.manager.create_task import CreateTaskUseCase
@@ -7,6 +8,8 @@ from src.use_cases.manager.get_all_tasks import GetAllTasksUseCase
 from src.use_cases.manager.get_by_task_by_id import GetTaskByIdUseCase
 from typing import Annotated
 from src.use_cases.manager.delete_task_by_id import DeleteTaskByIdUseCase
+from src.use_cases.manager.update_task import UpdateTaskUseCase
+from src.use_cases.user.add_task_comment import CreateCommentUseCase
 
 
 
@@ -113,4 +116,68 @@ async def delete_task_by_id(
         task_id=task_id,
         user_id=manager.id,
         user_team_id=manager.team_id
+    )
+
+
+
+@router.patch(
+    '/{task_id}',
+    description='[MANAGER] Изменение задачи',
+    summary='Изменить задачу',
+    status_code=status.HTTP_200_OK,
+    responses={
+        status.HTTP_200_OK: {
+            'description': 'Успешное обновление задачи',
+            'model': TaskOut,
+        },
+        status.HTTP_401_UNAUTHORIZED: {'description': 'Не авторизован'},
+        status.HTTP_403_FORBIDDEN: {'description': 'Недостаточно прав.'},
+        status.HTTP_400_BAD_REQUEST: {'description': 'Менеджер не состоит в команде.'}
+    },
+)
+@inject
+async def update_task(
+    task_id: Annotated[int, Path()],
+    updated_task: TaskUpdate,
+    manager: Depends[ManagerUserOut],
+    use_case: Depends[UpdateTaskUseCase]
+) -> TaskOut:
+    
+    return await use_case.execute(
+        updated_task=updated_task,
+        task_id=task_id,
+        team_id=manager.team_id,
+        author_id=manager.id
+    )
+
+
+
+@router.post(
+    '/{task_id}/comments',
+    description='Добавление комментария к задаче',
+    summary='Добавить комментарий',
+    status_code=status.HTTP_201_CREATED,
+    responses={
+        status.HTTP_201_CREATED: {
+            'description': 'Комментарий добавлен',
+            'model': TaskCommentOut,
+        },
+        status.HTTP_401_UNAUTHORIZED: {'description': 'Не авторизован'},
+        status.HTTP_404_NOT_FOUND: {'description': 'Задача не найдена'},
+        status.HTTP_403_FORBIDDEN: {'description': 'Нет доступа к задаче'},
+    },
+)
+@inject
+async def create_comment(
+    task_id: Annotated[int, Path()],
+    comment: TaskCommentCreate,
+    user: Depends[UserModel],
+    use_case: Depends[CreateCommentUseCase],
+) -> TaskCommentOut:
+    
+    return await use_case.execute(
+        task_id=task_id,
+        comment=comment,
+        author_id=user.id,
+        team_id=user.team_id
     )
